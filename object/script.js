8e9eb458f64cb92018e8b07bc1965a795e5f4b3f
dmasdi123 <febriandimas634@gmail.com>
2022-04-23 03:54:40 +0700

    prototype

--- object/script.js
+++ object/script.js
@@ -38,27 +38,26 @@
 // let febrian = Mahasiswa('Dimas', 20);
 
 //function declaration with object.create-------------------------
-const  methodMahasiswa = {
-    makan: function(porsi) {
-        this.energy += porsi;
-        console.log(`Hello ${this.nama}`);
-    }
+// const  methodMahasiswa = {
+//     makan: function(porsi) {
+//         this.energy += porsi;
+//         console.log(`Hello ${this.nama}`);
+//     },
+//     main: function(waktu) {
+//         this.energy -= waktu;
+//         console.log(`Halo ${this.nama}`);
+//     }
+// }
 
-    main: function(waktu) {
-        this.energy -= waktu;
-        console.log(`Halo ${this.nama}`);
-    }
-}
+// function Mahasiswa(nama, energy) {
+//     let mahasiswa = Object.create(methodMahasiswa);
+//     mahasiswa.nama = nama;
+//     mahasiswa.energy = energy;
 
-function Mahasiswa(nama, energy) {
-    let mahasiswa = Object.create(methodMahasiswa);
-    mahasiswa.nama = nama;
-    mahasiswa.energy = energy;
+//     return mahasiswa;
+// } 
 
-    return mahasiswa;
-} 
-
-let febrian = Mahasiswa('Dimas', 20);
+// let febrian = Mahasiswa('Dimas', 20);
 
 
 //contructor function--------------------------
@@ -80,3 +79,42 @@ let febrian = Mahasiswa('Dimas', 20);
 // }
 
 // let febrian = new Mahasiswa('Dimas', 20);
+
+
+//prototype-----------------------------------------------------
+// function Mahasiswa(nama, energy) {
+   
+//     this.nama = nama;
+//     this.energy = energy;
+
+// } 
+
+// Mahasiswa.prototype.makan = function (porsi) {
+//     this.energy += porsi;
+//     return `halo ${this.nama}`;
+// }
+
+// Mahasiswa.prototype.main = function (jam) {
+//     this.energy -= jam;
+//     return `halo ${this.nama}`;
+// }
+
+// let febrian = new Mahasiswa('Dimas', 20);
+
+//veri class------------------------------------
+class Mahasiswa {
+    constructor(nama, energy){
+        this.nama = nama;
+        this.energy = energy;
+    }
+
+    makan(porsi) {
+        this.energy += porsi;
+        return `halo ${this.nama}`;
+    }
+    main(jam) {
+        this.energy -= jam;
+        return `halo ${this.nama}`;
+    }
+}
+let febrian = new Mahasiswa('Dimas', 20);
