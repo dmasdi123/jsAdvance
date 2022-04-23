f45b4fb9578043897589e2d71ece9dc1e444a480
dmasdi123 <febriandimas634@gmail.com>
2022-04-24 04:12:08 +0700

    this pada arrow function

--- arrow function/script.js
+++ arrow function/script.js
@@ -22,11 +22,61 @@
 // console.log(jumlahHuruf);
 
 //example 3 hasil bentuk objek
-let mahasiswa = ['febrian', 'dimas', 'winaputra'];
-let jumlahHuruf = mahasiswa.map( (nama) => ({
-    nama: nama,
-    jmlHuruf: nama.length
-}));
+// let mahasiswa = ['febrian', 'dimas', 'winaputra'];
+// let jumlahHuruf = mahasiswa.map( (nama) => ({
+//     nama: nama,
+//     jmlHuruf: nama.length
+// }));
 
-console.log(jumlahHuruf);
+// console.log(jumlahHuruf);
 
+
+//konsep this pada arrow function--------------
+//constructor function
+// const Mahasiswa = function() {
+//     this.nama = 'febrian';
+//     this.umur = 22;
+//     this.sayHello = function() {
+//         console.log(`halo saya ${this.nama} umur ${this.umur}`);
+//     }
+// }
+
+// const dimas = new Mahasiswa();
+
+//versi arrow function, note: constructor tidak bisa dibuat menggunakan arrow function
+// const Mahasiswa = function() {
+//     this.nama = 'febrian';
+//     this.umur = 22;
+//     this.sayHello = () => {
+//         console.log(`halo saya ${this.nama} umur ${this.umur}`);
+//     }
+// }
+
+// const dimas = new Mahasiswa();
+
+//versi objeck literal
+// const mhs = {
+//     nama: 'febrian',
+//     umur: 27,
+//     sayHello: () => {
+//         console.log(`halo saya ${nama} umur ${umur}`);
+//     }
+// }
+
+
+
+//example with real project
+const box = document.querySelector('.box');
+box.addEventListener('click', function() {
+    let satu = 'size';
+    let dua = 'caption';
+
+    if (this.classList.contains(satu)) {
+        [satu, dua] = [dua, satu];
+    }
+
+    this.classList.toggle('size');
+    setTimeout( () => {
+        this.classList.toggle('caption');
+    }, 600);
+});
